82989a4ff42823a95ac89cb25bb664d2ad23c56c
Roberto cerquera <rcerquer@emeal.nttdata.com>
2025-06-10 21:18:47 -0500

    update

--- Dev/inventario_servidores_windows.py
+++ Dev/inventario_servidores_windows.py
@@ -224,8 +224,7 @@ def build_inventory(inventory_data, so_filter_to_add):
             'tipo_vmware': safe_get(inventory["TIPO_VMWARE"]),
             'vcenter_vmware': safe_get(inventory["VCENTER_VMWARE"]),
             'datacenter_vmware': safe_get(inventory["DATACENTER_VMWARE"]),
-            'vm_name': return_valid_text(inventory["VM_NAME"]),
-            'azure_resource_group': return_valid_text(inventory["azure_resource_group"]),
+            'vm_name': return_valid_text(inventory["VM_NAME"])
         }
         # Agregar hostvars
         ansible_inventory['_meta']['hostvars'][hostname] = data_host
